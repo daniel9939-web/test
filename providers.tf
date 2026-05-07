029419ced3b80cfd992b3d3895bb8e5a9fb3bca2
daniel9939-web <homin9939@gmail.com>
2026-05-07 12:05:03 +0900

    apply & destroy

--- providers.tf
+++ providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket = "todayisfridaymaybe"
+    key = "infra/terraform.tfstate"
+    region = "ap-northeast-2"
+  }
+}
+
+terraform {
   backend "local" {
     path = "./terraform.tfstate"
   }
